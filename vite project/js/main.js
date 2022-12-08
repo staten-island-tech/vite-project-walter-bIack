ad47e69b92d2134e795a58114e782aaf079eb273
walter-bIack <leon.selyomin@gmail.com>
2022-12-08 12:32:22 -0500

    t

---
+++ vite project/js/main.js
@@ -0,0 +1,7 @@
+import "../styles/style.css";
+import { menu } from "./menu";
+document.querySelector("#app").innerHTML = `
+  <h1>Hello?????</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+console.log(menu);
