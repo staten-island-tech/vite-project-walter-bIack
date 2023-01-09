1087dd14df71bef66f9ea28cbb78ef61d18eb946
walter-bIack <leon.selyomin@gmail.com>
2023-01-09 13:15:29 -0500

    e

--- vite-project/list.js
+++ vite-project/list.js
@@ -1,101 +1,113 @@
 export const menu = [
   {
     name: "Hog Rider",
-    price: 14,
-    description: "Juicy Burger",
+    elixer: 4,
+    description:
+      "spam this card at the bridge while adjusting your glasses and failing to convince anyone that it requires skill.",
     image:
       "https://th.bing.com/th/id/OIP.BXPFi-Mcwsry-wxlKxzOJAHaKh?w=126&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "beef",
+    Skill: "Mid",
     sales: ["monday", "thursday"],
   },
   {
     name: "Mega knight",
-    price: 13,
-    description: "Very Good Grilled Cheese",
+    elixer: 7,
+    description:
+      "People that play this card are still waiting for their dad to come home with the milk",
     image:
       "https://th.bing.com/th/id/OIP.6bkFZ997HfE05yhCU6EiBgHaEK?w=314&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "none",
+    Skill: "Fatherless",
     sales: ["tuesday", "friday"],
   },
   {
     name: "Golem",
-    price: 6,
-    description: "Crispy Fries",
+    elixer: 8,
+    description: "If you play this your brain is smoother then Frusci's head",
     image:
       "https://th.bing.com/th/id/OIP.MaOiHtMe9CuoNSzvRONXugHaDg?w=336&h=165&c=7&r=0&o=5&pid=1.7",
-    meat: "none",
+    Skill: "Fatherless",
     sales: ["saturday", "sunday"],
   },
   {
     name: "Elixer golem",
-    price: 9,
-    description: "Spicy Buffalo Wings",
+    elixer: 3,
+    description:
+      "Anyone who plays this lives in his mom's cheeto stained, musty smelling, takeout filled basement",
     image:
       "https://th.bing.com/th/id/OIP.xBzWrci5cbiXrRiu6QFuqwAAAA?w=237&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "chicken",
+    Skill: "Fatherless",
     sales: ["wednesday", "monday"],
   },
   {
     name: "Mother witch",
-    price: 8,
-    description: "Crispy Chicken Tenders",
+    elixer: 4,
+    description: "This card is HARAM. It literally turns people into pork",
     image:
       "https://th.bing.com/th/id/OIP.0foOtROuWj064V15oPMnjgHaNJ?w=187&h=333&c=7&r=0&o=5&pid=1.7",
-    meat: "chicken",
+    Skill: "Skill",
     sales: ["saturday", "thursday"],
   },
   {
     name: "Archer Queen",
-    price: 6,
-    description: "Made From Rats Caught in The Kitchen",
+    elixer: 5,
+    description: "The image speaks for itself",
     image:
       "https://th.bing.com/th/id/OIP.f-ViNTrrOBAh0lY4TEK5NgHaEK?w=300&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "beef",
+    Skill: "Mid",
     sales: ["friday", "thursday"],
   },
   {
     name: "Ice wizard",
-    price: 9,
-    description: "Salad with Chicken",
-    meat: "chicken",
+    elixer: 3,
+    description: "This man knows how to take his time. He doesnt rush",
+    Skill: "Skill",
     image:
       "https://th.bing.com/th/id/OIP.b7Vms9HjEPQ_tI2OJ-Ax2gHaHu?w=148&h=180&c=7&r=0&o=5&pid=1.7",
     sales: ["tuesday", "friday"],
   },
   {
     name: "Minor",
-    elixer: 0,
-    description: "Delicious Ice Cream",
+    elixer: 3,
+    description: "If I speak im in trouble.",
     image:
       "https://th.bing.com/th/id/R.060c1814ccf5f38ab0963391e98520f9?rik=yv5HTAdY%2fI8eUQ&pid=ImgRaw&r=0",
-    meat: "none",
+    Skill: "Skill",
     sales: ["tuesday", "tursday"],
   },
   {
     name: "Cannon cart",
-    elixer: 0,
-    description: "Delicious Ice Cream",
+    elixer: 5,
+    description: "Still slower then your nan in the chair",
     image:
       "https://th.bing.com/th/id/OIP.06jiU2DQXIczGeunkA77XQHaFW?w=230&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "none",
+    Skill: "Mid",
     sales: ["tuesday", "tursday"],
   },
   {
     name: "Royal Giant",
-    elixer: 0,
-    description: "Delicious Ice Cream",
+    elixer: 6,
+    description: "His rock solid balls do a lot of damamge",
     image:
       "https://th.bing.com/th/id/OIP.-PoJ25te0VTju_ZB2dWutwHaEK?w=294&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "none",
+    Skill: "Mid",
     sales: ["tuesday", "tursday"],
   },
   {
     name: "goblin",
-    elixer: 0,
-    description: "Delicious Ice Cream",
+    elixer: 2,
+    description: "average baddie in Ohio",
     image:
       "https://th.bing.com/th/id/OIP.FvRQw6JAFlAkVnFjd0xoVgEsEL?w=202&h=180&c=7&r=0&o=5&pid=1.7",
-    meat: "none",
+    Skill: "Skill",
+    sales: ["tuesday", "tursday"],
+  },
+  {
+    name: "rage",
+    elixer: 2,
+    description: "this spell slaps harder than your dads belt.",
+    image:
+      "https://th.bing.com/th/id/OIP.NjeIGUDIakGswW-t-Z4qvwHaD-?w=329&h=180&c=7&r=0&o=5&pid=1.7",
+    Skill: "Fatherless",
     sales: ["tuesday", "tursday"],
   },
 ];
--- vite-project/main.js
+++ vite-project/main.js
@@ -35,17 +35,17 @@ document.querySelector("#all").addEventListener("click", function () {
 });
 document.querySelector("#beef").addEventListener("click", function () {
   menuDiv.innerHTML = "";
-  const filtered = menu.filter((item) => item.meat === "beef");
+  const filtered = menu.filter((item) => item.Skill === "beef");
   display(filtered);
 });
 document.querySelector("#chicken").addEventListener("click", function () {
   menuDiv.innerHTML = "";
-  const filtered = menu.filter((item) => item.meat === "chicken");
+  const filtered = menu.filter((item) => item.Skill === "chicken");
   display(filtered);
 });
 document.querySelector("#vegetarian").addEventListener("click", function () {
   menuDiv.innerHTML = "";
-  const filtered = menu.filter((item) => item.meat === "none");
+  const filtered = menu.filter((item) => item.Skill === "none");
   display(filtered);
 });
 
@@ -59,16 +59,16 @@ document.querySelector("#theme-button").addEventListener("click", function () {
 
 function display(array) {
   array.forEach((item) => {
-    if (item.sales.includes(day)) item.price = item.price / 2;
+    if (item.sales.includes(day)) item.elixer = item.elixer / 2;
     menuDiv.insertAdjacentHTML(
       "beforeend",
       `<div class="menu-object">
                 <h2>${item.name}</h2>
                 <img class="menu-image" src="${item.image}">
-                <p class="menu-object-price">$${item.price}</p>
+                <p class="menu-object-price">${item.elixer}</p>
                 <p class="menu-object-text">${item.description}</p>
             </div>`
     );
-    if (item.sales.includes(day)) item.price = item.price * 2;
+    if (item.sales.includes(day)) item.elixer = item.elixer * 2;
   });
 }
